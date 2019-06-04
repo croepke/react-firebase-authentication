e1c64cc985942490cc4f920f7e22379c458aed25
Christian Röpke <croepke@posteo.de>
2019-06-04 09:06:41 +0200

    feat: Add signup

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,5 +1,5 @@
-import FirebaseContext from './context';
+import FirebaseContext, { withFirebase } from './context';
 import Firebase from './firebase';
 
 export default Firebase;
-export { FirebaseContext };
+export { FirebaseContext, withFirebase };
